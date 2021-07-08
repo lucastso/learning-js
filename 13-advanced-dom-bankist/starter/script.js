7ffcce4761616d6c3815d555e94a2f31c03c1d79
lucastso <lucastassisouza@gmail.com>
2021-07-08 14:15:13 -0300

    feat: selecting, creating and deleting elements

--- 13-advanced-dom-bankist/starter/script.js
+++ 13-advanced-dom-bankist/starter/script.js
@@ -30,4 +30,38 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-/////////////////////////////////////// LECTURE: HOW DOM REALLY WORKS
+/////////////////////////////////////// LECTURE: SELECTING, CREATING AND DELETING ELEMENTS
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header'); // select the first element
+const allSections = document.querySelectorAll('.section'); // select multiple elements
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// delete elements
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  // message.remove(); or L
+  message.parentElement.removeChild(message);
+});
